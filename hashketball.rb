ff84aa33b3913b9b62d3bc1f911f871345e2ffd8
vivacious-revert-7013 <mesmith454@gmail.com>
2020-11-08 21:20:49 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -69,14 +69,14 @@ def game_hash
       :colors => ["Turquoise", "Purple"],
       :players => [
         {
-        :player_name => "Alan Anderson",
-        :number => 0,
-        :shoe => 16,
-        :points => 22,
-        :rebounds => 12,
-        :assits => 12,
-        :steals => 3,
-        :blocks => 1,
+        :player_name => "Jeff Adrien",
+        :number => 4,
+        :shoe => 18,
+        :points => 10,
+        :rebounds => 1,
+        :assits => 1,
+        :steals => 2,
+        :blocks => 7,
         :slam_dunks => 1 
         }, 
         {
