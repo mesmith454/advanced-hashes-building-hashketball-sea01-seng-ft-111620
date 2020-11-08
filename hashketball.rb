814cb8df130c75d9aa1111a1094094e3aa90791e
vivacious-revert-7013 <mesmith454@gmail.com>
2020-11-08 21:15:25 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -27,7 +27,17 @@ def game_hash
         :blocks => 12,
         :slam_dunks => 7 
         },
-        
+         {
+        :player_name => "Brook Lopez",
+        :number => 11,
+        :shoe => 17,
+        :points => 17,
+        :rebounds => 19,
+        :assits => 10,
+        :steals => 3,
+        :blocks => 1,
+        :slam_dunks => 1 
+        },
       ]
     }, 
     :away => 
