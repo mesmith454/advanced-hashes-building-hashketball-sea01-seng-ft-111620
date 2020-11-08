af29496958d1ed50668e27a1ee3a524591cc04cc
vivacious-revert-7013 <mesmith454@gmail.com>
2020-11-08 21:13:01 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,18 +4,18 @@ def game_hash
     {
       :team_name => "Brooklyn Nets",
       :colors => ["Black", "White"],
-      :players => 
-      {
+      :players => [
+      
         :player_name => "Alan Anderson",
         :number => 0,
         :shoe => 16,
         :points => 22,
         :rebounds => 12,
         :assits => 12,
-        :steals => ,
-        :blocks => ,
-        :slam_dunks => 
-      },
+        :steals => 3,
+        :blocks => 1,
+        :slam_dunks => 1 
+      ]
     }, 
     :away => 
       {
