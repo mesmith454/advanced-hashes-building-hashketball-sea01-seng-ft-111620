da459f18a0586ce46c8d85e9a4d3cae2004a6aae
vivacious-revert-7013 <mesmith454@gmail.com>
2020-11-08 21:01:14 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,9 +2,14 @@ def game_hash
   {
     :home => 
     {
-      :team_name => ""
-      :colors => []
+      :team_name => "",
+      :colors => [],
+      :players => {}
+    },
+     :away => 
+    {
+      :team_name => "",
+      :colors => [],
       :players => {}
     } 
-     
   }
